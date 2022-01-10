e21f78e949c5196855bf113685e1317aa0e92ac1
Liv <lopez.olivia17@outlook.com>
2022-01-10 16:09:27 -0600

    modified functions to get game function to calculate rounds right...still goes through 5 times without checking for new input from user and computer.

--- script.js
+++ script.js
@@ -4,62 +4,91 @@
   // the user is then prompted for their input of rock/paper/scissors 
   // compare the two and based on that winner is selected 
 
+// need a variable to hold the results of the game 
+let result = null;
+
 // function for the computer selection - randomly select string 'rock', 'paper', or 'scissors'
 function computerPlay(){
   // array of string options 
-  const computer = ['Rock', 'Paper', 'Scissors'];
+  const computerOptions = ['Rock', 'Paper', 'Scissors'];
 
   // use math random to randomly choose a number 
-  let randomNumber = Math.floor(Math.random() * 3)
+  let randomNumber = Math.floor(Math.random() * computerOptions.length)
 
-  // based on the randomNumber return an element from the computerSelection array
-  // console.log(computerSelection.at(randomNumber))
-  return computer.at(randomNumber)
+  // based on the randomNumber return an element from the computerOptions array
+  // console.log(computerOptions[randomNumber])
+
+  let compSelection = computerOptions[randomNumber].toLocaleLowerCase()
+  return compSelection
 }
 
 // function that plays a single round of rock, paper, scissors and takes in parameters - the computers selection and the plays selection 
 function playRound(playerSelection, computerSelection){
-  // lowercase both the computer choice and the users selection so there are no errors 
-  let playerSelectionLowerCased = playerSelection.toLowerCase();
-  console.log(playerSelectionLowerCased)
-  let computerSelectionLowerCased = computerSelection.toLowerCase();
-  console.log(computerSelectionLowerCased)
 
-  // need a variable to hold the results of the game 
-  let result = ''
+  // lowercase both the computer choice and the users selection so there are no errors 
+  // let playerSelectionLowerCased = playerSelection.toLowerCase();
+  // console.log(playerSelectionLowerCased)
+  // let computerSelectionLowerCased = computerSelection.toLowerCase();
+  // console.log(computerSelectionLowerCased)
 
   // compare the computer's choice with the user's choice 
-  if(playerSelectionLowerCased === computerSelectionLowerCased){
-    console.log('Tie!')
+  if(playerSelection === computerSelection){
+    //console.log('Tie!')
     result = 'Tie!'
   } 
 
-  if(playerSelectionLowerCased === 'rock' && computerSelectionLowerCased === 'scissors'){
-    console.log('You win!')
+  if(playerSelection === 'rock' && computerSelection === 'scissors'){
+    //console.log('You win!')
     result = 'You win!'
-  } else if (playerSelectionLowerCased === 'rock' && computerSelectionLowerCased === 'paper'){
-    console.log('You Lose!')
+  } else if (playerSelection === 'rock' && computerSelection === 'paper'){
+    //console.log('You Lose!')
     result = 'You Lose!'
-  } else if(playerSelectionLowerCased === 'paper' && computerSelectionLowerCased === 'scissors'){
-    console.log('You Lose!')
+  } else if(playerSelection === 'paper' && computerSelection === 'scissors'){
+    //console.log('You Lose!')
     result = 'You Lose!'
-  } else if(playerSelectionLowerCased === 'paper' && computerSelectionLowerCased === 'rock'){
-    console.log('You Win!')
+  } else if(playerSelection === 'paper' && computerSelection === 'rock'){
+    //console.log('You Win!')
     result = 'You win!'
-  } else if(playerSelectionLowerCased === 'scissors' && computerSelectionLowerCased === 'paper'){
-    console.log('You win!')
+  } else if(playerSelection === 'scissors' && computerSelection === 'paper'){
+    //console.log('You win!')
     result = 'You win!'
-  } else if (playerSelectionLowerCased === 'scissors' && computerSelectionLowerCased === 'rock'){
-    console.log('You Lose!')
+  } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+    //console.log('You Lose!')
     result = 'You Lose!'
   }
 
-  // return a string letting the user know who wins or loses 
   console.log(result)
   return result
 }
 
+function game() {
+  let comp = 0
+  let player = 0
+
+  // call function 5 times for the game 
+   for(i = 0; i < 5; i++){
+    playRound(playerSelection, computerSelection)
+
+    if(result === 'You win!'){
+      player++
+    } else if (result === 'You Lose!'){
+      comp++
+    } 
+   }
+
+console.log(comp)
+console.log(player)
+}
+
+function usersPlay() {
+  let userInput = prompt("Rock, Paper, or Scissors?")
+
+  let lowerCased = userInput.toLocaleLowerCase()
+  return lowerCased
+}
+
+
 // hard coded player selection for testing purposes 
-const playerSelection = 'rock';
-const computerSelection = computerPlay();
-playRound(playerSelection, computerSelection)
+let playerSelection = usersPlay();
+let computerSelection = computerPlay();
+game()
